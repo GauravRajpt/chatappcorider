1d0c4d258e9bba1f7cd120311b9a99d078af3570
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-20 14:34:17 +0530

    fix the problume

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@ import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import axios from 'axios';
 
+const App = () => {
+  const [userData, setUserData] = useState([]);
 
-import "./App.css";
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://3.111.128.67/assignment/chat?page=0");
+      setUserData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-function App() {
-
-  const [userData, setUserData]= useState([]);
-
-
-  useEffect(()=>{
-    
-    async function getData(){
-      const data=  await fetch('http://3.111.128.67/assignment/chat?page=0');
-      const json=  await data.json();
-      
-      setUserData(json);
-      }
-   getData();
-  },[])
   return (
     <div className="bg-[#FAF9F4] h-full w-[100vw] sm:max-w-[450px] sm:m-auto relative flex flex-col  ">
-     <Header userData={userData}/>
-     <Body userData={userData}/>
-     <Footer userData={userData}/>
+      <Header userData={userData} />
+      <Body userData={userData} />
+      <Footer userData={userData} />
     </div>
   );
 }
