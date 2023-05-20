aee9027ef8c47b8374a7bf354018ac6ca37366b8
GauravRajpt <joygrajput@gmail.com>
2023-05-20 12:40:23 +0530

    initiall

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,32 +1,45 @@
-const CACHE_NAME = "version 1";
-const urlstocache = ["index.html", "offline.html"];
+// const CACHE_NAME = "version 1";
+// const urlstocache = [];
 
-const self = this;
+// const self = this;
 
-//install SW
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME)
-    .then((cache)=>{
-        console.log('opened cache');
+// //install SW
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("opened cache");
 
-        return cache.addAll(urlstocache);
-    }));
-});
+//       return cache.addAll(urlstocache);
+//     })
+//   );
+// });
 
-//listen of request
+// // //listen of request
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(()=>{
-            return fetch(event.request)
-                .catch(()=>{
-                    caches.match("offline.html")
-                })
-        })
-    )
-});
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request)
+//       .catch(()=>caches.match("offline.html"))
+      
+//     })
+//   );
+// });
 
-//Activate the SW
+// //Activate the SW
 
-self.addEventListener("activate", (event) => {});
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [];
+//   cacheWhitelist.push(CACHE_NAME);
+//   caches.keys().then((cacheNames) =>
+//     Promise.all(
+//       cacheNames.map((cacheName) => {
+//         if (!cacheWhitelist.includes(cacheName)) {
+//           console.log(cacheName);
+//           return caches.delete(cacheName);
+//         }
+//         return null
+//       })
+//     )
+//   );
+// });
--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
-import "./App.css";
 
+import "./App.css";
 
 
 function App() {
